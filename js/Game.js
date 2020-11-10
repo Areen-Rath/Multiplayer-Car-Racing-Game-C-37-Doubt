9f794eca0b3ce8b1c8f02388f28ee8c71d075661
Areen-Rath <70901420+Areen-Rath@users.noreply.github.com>
2020-11-10 20:01:13 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,54 @@
+class Game {
+  constructor(){
+
+  }
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    fill("blue");
+    text("Game Started. Enjoy!!!", 100, 100);
+    Player.getPlayerInfo();
+    if(allPlayers !== undefined){
+      var displayPosition = 130;
+      for(var plr in allPlayers){
+        if(plr === "player" + player.index){
+          fill("green");
+        } else {
+          fill("blue");
+        }
+        displayPosition += 20;
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120, displayPosition);
+      }
+    }
+
+    if(keyDown("UP_ARROW") && player.index !== null){
+      player.distance += 20;
+      player.update()
+    }
+  }
+}
